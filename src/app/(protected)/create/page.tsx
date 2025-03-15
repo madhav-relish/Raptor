176f30bea443d324208e04998dd0b2b6d268387d
Madhav <madhavsingh.relish@gmail.com>
2025-03-15 14:04:10 +0530

    add useRefetch hook to enable query refetching after project creation

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import useRefetch from '@/hooks/use-refetch';
 import { api } from '@/trpc/react';
 import React from 'react'
 import { useForm } from 'react-hook-form';
@@ -17,6 +18,7 @@ type FormInput = {
 const CreatePage = () => {
   const createProject = api.project.createProject.useMutation()
   const {register, handleSubmit, reset} = useForm<FormInput>()
+const refetch = useRefetch()
 
   function onSubmit (data: FormInput){
     window.alert(JSON.stringify(data))
@@ -27,6 +29,7 @@ const CreatePage = () => {
     },{
       onSuccess:()=>{
         toast.success("Project created successfully")
+        refetch()
         reset()
       },
       onError:()=>{
