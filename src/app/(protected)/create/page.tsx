2c4da18343e7f5957492b04ba19a1284f0aef9b5
Madhav <madhavsingh.relish@gmail.com>
2025-03-15 12:56:57 +0530

    add create project page and API router with authentication middleware

---
+++ src/app/(protected)/create/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { api } from '@/trpc/react';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+type FormInput = {
+    repoUrl: string;
+    projectName: string;
+    githubToken?: string
+}
+
+
+
+const CreatePage = () => {
+  const createProject = api.project.createProject.useMutation()
+  const {register, handleSubmit, reset} = useForm<FormInput>()
+
+  function onSubmit (data: FormInput){
+    window.alert(JSON.stringify(data))
+    createProject.mutate({
+      githubUrl: data.repoUrl,
+      name: data.projectName,
+      githubToken: data.githubToken
+    },{
+      onSuccess:()=>{
+        toast.success("Project created successfully")
+        reset()
+      },
+      onError:()=>{
+        toast.error("Error while creating project")
+      }
+    })
+     return true
+   }
+
+  return (
+    <div className='flex items-center gap-12 h-full justify-center'>
+      <img src='/undraw_github.svg' alt='github' className='h-56 w-auto'/>
+    <div>
+      <div>
+        <h1>Link your github repository</h1>
+        <p>Enter the url to link you repository</p>
+      </div>
+      <div className='h-4'>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+            {...register("projectName", {required: true})}
+            required
+            placeholder='Project Name'
+            />
+             <Input
+            {...register("repoUrl", {required: true})}
+            required
+            type='url'
+            placeholder='Github UrL'
+            />
+              <Input
+            {...register("githubToken")}
+          placeholder='Github Token (Optional)'
+            />
+            <div className='h-4'></div>
+            <Button type='submit'>Create Project</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default CreatePage
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -13,6 +13,7 @@ import { ZodError } from "zod";
 
 import { auth } from "@/server/auth";
 import { db } from "@/server/db";
+import { useSession } from "next-auth/react";
 
 /**
  * 1. CONTEXT
@@ -84,6 +85,22 @@ export const createTRPCRouter = t.router;
  * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
  * network latency that would occur in production but not in local development.
  */
+
+const isAuthenticated = t.middleware(async ({next, ctx})=>{
+  const user = await useSession()
+  if(!user){
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'You much be logged in to access this resource'
+    })
+  }
+  return next({
+    ctx:{
+      ctx,
+      user
+    }
+  });  
+})
 const timingMiddleware = t.middleware(async ({ next, path }) => {
   const start = Date.now();
 
@@ -118,16 +135,6 @@ export const publicProcedure = t.procedure.use(timingMiddleware);
  *
  * @see https://trpc.io/docs/procedures
  */
-export const protectedProcedure = t.procedure
-  .use(timingMiddleware)
-  .use(({ ctx, next }) => {
-    if (!ctx.session || !ctx.session.user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-      ctx: {
-        // infers the `session` as non-nullable
-        session: { ...ctx.session, user: ctx.session.user },
-      },
-    });
-  });
+export const protectedProcedure = t.procedure.use(isAuthenticated)
+  
+
