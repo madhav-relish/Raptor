3cf067cfb562ef9fcf73057912612d1986a0bee5
Madhav <madhavsingh.relish@gmail.com>
2025-03-16 23:02:20 +0530

    refactor: remove InputOTP component and update project router for optional session handling

--- src/server/api/routers/project.tsx
+++ src/server/api/routers/project.tsx
@@ -3,46 +3,53 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { pollCommits } from "@/lib/github";
 
 export const projectRouter = createTRPCRouter({
-    createProject: protectedProcedure.input(
-        z.object({
-            name: z.string(),
-            githubUrl: z.string(),
-            githubToken: z.string().optional()
-        })
-    ).mutation(async ({ctx, input})=>{
-        const project = await ctx.db.project.create({
-            data:{
-                githubUrl: input.githubUrl,
-                name: input.name,
-                UserToProject:{
-                    create:{
-                        userId: ctx.user.user.id
-                    }
-                }
-            }
-        })
-        await pollCommits(project.id)
-       return project
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        githubUrl: z.string(),
+        githubToken: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          githubUrl: input.githubUrl,
+          name: input.name,
+          UserToProject: {
+            create: {
+              userId: ctx.user.user.id,
+            },
+          },
+        },
+      });
+      await pollCommits(project.id);
+      return project;
     }),
-    getAllProjects: protectedProcedure.query(async ({ctx})=>{
-        return await ctx.db.project.findMany({
-            where:{
-                UserToProject: {
-                    some:{
-                        userId: ctx.user.user.id
-                    }
-                },
-                deletedAt: null
-            }
-        })
+  getAllProjects: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.project.findMany({
+      where: {
+        UserToProject: {
+          some: {
+            userId: ctx.user.user.id,
+          },
+        },
+        deletedAt: null,
+      },
+    });
+  }),
+  getCommits: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      pollCommits(input.projectId).then().catch(console.error);
+      return await ctx.db.commit.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+      });
     }),
-    getCommits: protectedProcedure.input(z.object({
-        projectId: z.string()
-    })).query(async({ctx, input})=>{
-        return await ctx.db.commit.findMany({
-            where: {
-                projectId: input.projectId
-            }
-        })
-    })
-})
+});
