55ecea33e2fb3af251fbb8eccc84d3e079777e64
Madhav <madhavsingh.relish@gmail.com>
2025-03-18 18:38:06 +0530

    refactor: remove console log for API key in gemini.ts

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import { commitSummaryPrompt } from './constants'
 import { Document } from '@langchain/core/documents';
-console.log('API KEY::', process.env.GEMINI_API_KEY)
-
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
