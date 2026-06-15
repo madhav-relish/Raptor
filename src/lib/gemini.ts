4c7e0c820bdacffe3b1463b0e58e01d14252417c
Madhav <madhavsingh.relish@gmail.com>
2026-06-15 14:14:32 +0530

    Moved to gemini-2.5-flash

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -3,7 +3,7 @@ import { commitSummaryPrompt } from './constants'
 import { Document } from '@langchain/core/documents';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 
 export const aiSummariesCommit = async (diff: string) => {
