4e9888b59f6652bae956c02d63e20e5e9182d9f8
Madhav <madhavsingh.relish@gmail.com>
2026-06-15 14:09:55 +0530

    Migrated from gemini-1.5-flash to gemini-flash-latest

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -3,7 +3,7 @@ import { commitSummaryPrompt } from './constants'
 import { Document } from '@langchain/core/documents';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 
 export const aiSummariesCommit = async (diff: string) => {
