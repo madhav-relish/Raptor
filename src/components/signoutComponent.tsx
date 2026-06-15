ef706452f0be9d7f2819cfe783c4509ab183cb76
Madhav <madhavsingh.relish@gmail.com>
2026-06-15 16:07:38 +0000

    Added signout redirect

--- src/components/signoutComponent.tsx
+++ src/components/signoutComponent.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const SignOutButton = () => {
+  const router = useRouter()
+  const handleSignOut = () => {
+    try {
+      signOut();
+      router.push("/signin");
+    } catch (error) {
+      console.error("Error signing out:", error);
+
+    }
+  }
   return (
     <button
-      onClick={() => signOut()}
+      onClick={handleSignOut}
       className="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-600"
     >
       Sign Out
